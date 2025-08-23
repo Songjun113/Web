5f67eeee39cf8add37989fc6bc01f9d8f91e6d59
Songjun <131598485+Songjun113@users.noreply.github.com>
2025-08-23 15:44:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '松菌君', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '生物医学工程专业就读，初窥门径', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://songjj.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://songjundu.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
